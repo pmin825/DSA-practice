3a40c177770cabd277404ad21f79d4c2efdc93ac
pmin825 <pmin.0825@gmail.com>
2020-12-03 15:14:55 -0800

    branch sums problem

---
+++ branch_sums.js
@@ -0,0 +1,32 @@
+// This is the class of the input root.
+// Do not edit it.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function branchSums(root) {
+	let sums = [];
+	branchSumsHelper(root, 0, sums)
+	return sums;
+}
+
+function branchSumsHelper(node, runningSum, sums) {
+	if (!node) return;
+	let newRunningSum = runningSum + node.value;
+	if (!node.left && !node.right) {
+		sums.push(newRunningSum);
+		return;
+	}
+	
+	branchSumsHelper(node.left, newRunningSum, sums);
+	branchSumsHelper(node.right, newRunningSum, sums);
+}
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.branchSums = branchSums;
+
