977b2c22db4373da6f5ddd3c74a8df17c2709e5f
pmin825 <pmin.0825@gmail.com>
2020-12-08 16:41:36 -0800

    smallest difference

---
+++ smallest_difference.js
@@ -0,0 +1,25 @@
+// O(nlog(n)) time | O(1) space 
+
+function smallestDifference(arrayOne, arrayTwo) {
+    let idxOne = 0;
+    let idxTwo = 0;
+    let smallest = Infinity;
+    let final;
+    while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
+        let numOne = arrayOne[idxOne];
+        let numTwo = arrayTwo[idxTwo];
+        let current = Math.abs(numOne - numTwo)
+        idxTwo++
+        if (current < smallest) {
+            smallest = current;
+            final = [numOne, numTwo]
+        }
+        if (idxTwo === arrayTwo.length) {
+            idxTwo = 0
+            idxOne++
+        }
+    }
+    return final
+}
+// Do not edit the line below.
+exports.smallestDifference = smallestDifference;
