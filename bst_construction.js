182f6842a4c9bb5ff911aeef60950cbe89fe0e8b
pmin825 <pmin.0825@gmail.com>
2020-12-19 14:56:41 -0800

    bst construction

--- bst_construction.js
+++ bst_construction.js
@@ -6,55 +6,81 @@ class BST {
   }
 
   insert(value) {
-    let currentNode = this;
-		while (true) {
-			if (value < currentNode.value) {
-				if (currentNode.left === null) {
-					currentNode.left = new BST(value);
-					break
-				}	else {
-					currentNode = currentNode.left;
-				}
+  	if (value < this.value) {
+			if (this.left === null) {
+				this.left = new BST(value)
 			}	else {
-				if (currentNode.right === null) {
-					currentNode.right = new BST(value);
-					break;
-				}	else {
-					currentNode = currentNode.right;
-				}
+				this.left.insert(value);
+			}
+		}	else {
+			if (this.right === null) {
+				this.right = new BST(value)
+			}	else {
+				this.right.insert(value);
 			}
 		}
 		return this;
-	}
+  }
 
   contains(value) {
-    let currentNode = this;
-		while (true) {
-			if (currentNode.value === value) {
-				return true;
-			}	else if (currentNode.value > value) {
-				if (currentNode.left === null) {
-					return false;
+    if (value < this.value) {
+			if (this.left === null) {
+				return false;
+			}	else {
+				return this.left.contains(value);
+			}
+		}	else if (value > this.value) {
+			if (this.right === null) {
+				return false;
+			}	else {
+				return this.right.contains(value);
+			}
+		}	else {
+			return true;
+		}
+  }
+
+  remove(value, parent = null) {
+    if (value < this.value) {
+			if (this.left !== null) {
+				this.left.remove(value, this);
+			}
+		}	else if (value > this.value) {
+			if (this.right !== null) {
+				this.right.remove(value, this);
+			}
+		}	else {
+			if (this.left !== null && this.right !== null) {
+				this.value = this.right.getMinValue();
+				this.right.remove(this.value, this)
+			}	else if (parent === null) {
+				if (this.left !== null) {
+					this.value = this.left.value;
+					this.right = this.left.right;
+					this.left = this.left.left;
+				}	else if (this.right !== null) {
+					this.value = this.right.value;
+					this.left = this.right.left;
+					this.right = this.right.right;
 				} else {
-					currentNode = currentNode.left;
+					
 				}
-			}	else if (currentNode.value < value) {
-				if (currentNode.right === null) {
-					return false;
-				}	else {
-					currentNode = currentNode.right;
-				}
+			}	else if (parent.left === this) {
+				parent.left = this.left !== null ? this.left : this.right;
+			}	else if (parent.right === this) {
+				parent.right = this.left !== null ? this.left : this.right;
 			}
 		}
-		return false;
-  }
-
-  remove(value) {
-    // Write your code here.
-    // Do not edit the return statement of this method.
     return this;
   }
-}
 
-// Do not edit the line below.
-exports.BST = BST;
+  	getMinValue() {
+		if (this.left === null) {
+			return this.value;
+		}	else {
+			return this.left.getMinValue()
+		}
+	}
+	
+	
+}
