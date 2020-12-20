10a55adf59f02734c5065eb28a8214e2ec294363
pmin825 <pmin.0825@gmail.com>
2020-12-19 16:23:47 -0800

    fibonacci recursive and iterative

---
+++ fibonacci.js
@@ -0,0 +1,22 @@
+//recursive
+
+function fib(n) {
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fib(n-1) + fib(n-2);    
+}
+
+//iterative
+
+function fib(n) {
+    const fibs = [0, 1];
+    let counter = 2;
+    while (counter <= n) {
+        let temp = fibs[1];
+        fibs[1] = fibs[0] + fibs[1];
+        fibs[0] = temp;
+        counter++
+    }
+    if (n < 1) return fibs[0]
+    return fibs[1];
+}
