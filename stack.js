d7a593a651a1227f9b19a05673467f2b440d0c68
pmin825 <pmin.0825@gmail.com>
2020-12-19 19:22:21 -0800

    stack

---
+++ stack.js
@@ -0,0 +1,17 @@
+class Stack {
+    constructor(){
+        this.data = [];
+    }
+    
+    push(value){
+        this.data.push(value);
+    }
+    
+    pop(){
+        return this.data.pop();
+    }
+    
+    peek(){
+        return this.data[this.data.length - 1]
+    }
+}
