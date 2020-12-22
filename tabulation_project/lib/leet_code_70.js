9f4d8ec39388e3023f3bf5670b6eb09e4f86cdc2
pmin825 <pmin.0825@gmail.com>
2020-12-21 18:28:13 -0800

    climb stairs

--- tabulation_project/lib/leet_code_70.js
+++ tabulation_project/lib/leet_code_70.js
@@ -2,5 +2,16 @@
 // Feel free to use this file for scratch work.
 
 function climbStairs(n) {
-
+    let steps = [1, 2]
+    let ways = new Array(n + 1).fill(0)
+    ways[0] = 1
+    
+    for (const step in steps) {
+        for (let i = 0; i < n + 1; i++) {
+            if (step <= i) {
+                ways[i] += ways[i - step];
+            }
+        }
+    }
+    return ways[n];
 }
