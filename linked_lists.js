48ceff3a2bb5cdf9f229778beecc4cfb0443ee05
pmin825 <pmin.0825@gmail.com>
2020-12-23 14:35:42 -0800

    linked lists

--- linked_lists.js
+++ linked_lists.js
@@ -58,4 +58,46 @@ class LinkedList {
         }
         prev.next = null;
     }
-}
+    
+    insertLast(data) {
+        const last = this.getLast();
+        if (last) {
+            last.next = new Node(data)
+        }   else {
+            this.head = new Node(data)
+        }
+    }
+    
+    getAt(index) {
+        let count = 0;
+        let node = this.head
+        while (node) {
+            if (count === index) return node;
+            count++
+            node = node.next
+        }
+        return null;
+    }
+    
+    removeAt(index) {
+        if (!this.head) return;
+        if (index === 0) this.head = this.head.next;
+        
+        let prevNode = this.getAt(index - 1);
+        if (!prevNode || !prevNode.next) return;
+        prevNode.next = prevNode.next.next;
+    }
+    
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data)
+            return
+        }
+        if (index === 0) {
+            this.head = new Node(data, this.head)
+        }
+       
+        const prevNode = this.getAt(index - 1) || this.getLast();
+        const node = new Node(data, prevNode.next);
+        prevNode.next = node;
+    }
