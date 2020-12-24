5ae94e35c7f9df553256f5faf3f12eebf13bc29f
pmin825 <pmin.0825@gmail.com>
2020-12-24 14:57:44 -0800

    dfs traversal

---
+++ bfs_traversal.js
@@ -0,0 +1,34 @@
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  
+  traverseBF(fn) {
+      let node = this.root;
+      let queue = [node];
+      
+      while (queue.length) {
+          let currentNode = queue.shift();
+          queue.push(...currentNode.children)
+          fn(currentNode);
+      }
+  }
+}
+
+// You shouldn't need to modify the Node class
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter((node) => {
+      return node.data !== data;
+    });
+  }
+}
