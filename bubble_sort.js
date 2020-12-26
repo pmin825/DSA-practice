6d2b64f7fb3a5f55d5dd0bf1d7cefd69f17107f1
pmin825 <pmin.0825@gmail.com>
2020-12-26 12:47:08 -0800

    bubble sort 2

--- bubble_sort.js
+++ bubble_sort.js
@@ -16,5 +16,22 @@ function bubbleSort(array) {
 	return array;
 }
 
+function bubbleSort(arr) {
+    let sorted = false;
+    
+    while (!sorted) {
+        sorted = true;
+        for (let i = 0 ; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                sorted = false;
+            }
+        }
+    }
+    return arr;
+}
+
 // Do not edit the line below.
 exports.bubbleSort = bubbleSort;
