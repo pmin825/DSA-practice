71e6ac5b97f368235ee4dc1c73e76d1a147fe404
pmin825 <pmin.0825@gmail.com>
2020-12-31 16:58:36 -0800

    single cycle check

--- single_cycle_check.js
+++ single_cycle_check.js
@@ -1,14 +1,12 @@
 function hasSingleCycle(array) {
-	let count = 0;
 	let idx = 0;
-	let newIdx;
-
-	while (count < array.length) {
+	let count = 0;
+  while (count < array.length) {
 		if (count > 0 && idx === 0) return false;
 		count++
-		idx = getNextIdx(idx, array)
+		idx = getNextIdx(idx, array);
 	}
-	return idx === 0
+	return idx === 0;
 }
 
 function getNextIdx(idx, array) {
@@ -16,3 +14,6 @@ function getNextIdx(idx, array) {
 	const nextIdx = (idx + jump) % array.length;
 	return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
 }
+
+// Do not edit the line below.
+exports.hasSingleCycle = hasSingleCycle;
