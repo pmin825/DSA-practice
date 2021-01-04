0793d2daff7512724e8eefea24ea60ef049e6556
pmin825 <pmin.0825@gmail.com>
2021-01-04 15:03:06 -0800

    group anagrams

---
+++ group_anagrams.js
@@ -0,0 +1,15 @@
+function groupAnagrams(words) {
+	let hash = {} // {}
+  for (const word of words) {
+		let sortWord = word.split("").sort().join("")
+		if (!hash[sortWord]) {
+			hash[sortWord] = [word]
+		}	else {
+			hash[sortWord].push(word);
+		}
+	}
+	return Object.values(hash);
+}
+
+// Do not edit the line below.
+exports.groupAnagrams = groupAnagrams;
