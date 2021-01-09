e0d663392d77cba626928a854a58e9e9f338320e
pmin825 <pmin.0825@gmail.com>
2021-01-09 14:39:09 -0800

    rotate matrix flip

---
+++ rotate_matrix_flip.js
@@ -0,0 +1,30 @@
+// let matrix =
+// [
+//   [ 1,  2,  3, 4],
+//   [ 5,  6,  7, 8],
+//   [ 9, 10, 11, 12],
+//   [13, 14, 15, 16]
+// ]
+
+// Output:
+// [
+//  [13,  9, 5, 1],
+//  [14, 10, 6, 2],
+//  [15, 11, 7, 3],
+//  [16, 12, 8, 4]
+// ]
+
+function rotateMatrix(matrix) {
+  let length = matrix.length;
+
+  for (let row = 0; row < Math.floor(length / 2); row++) {
+    for (let col = 0; col < length; col++) {
+      let temp = matrix[row][col];
+      matrix[row][col] = matrix[length - 1 - row][length - 1 - col];
+      matrix[length - 1 - row][length - 1 - col] = temp;
+    }
+  }
+  return matrix;
+}
+
+rotateMatrix(matrix);
