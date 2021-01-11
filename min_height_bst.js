cdff4d3223f265390dfceb1fae945a58ae197f4a
pmin825 <pmin.0825@gmail.com>
2021-01-10 17:24:26 -0800

    min height bst alternate solution

--- min_height_bst.js
+++ min_height_bst.js
@@ -45,3 +45,45 @@ class BST {
 
 // Do not edit the line below.
 exports.minHeightBst = minHeightBst;
+
+// O(n) time || O(n) space
+
+function minHeightBst(array) {
+    return constructMinHeightBst(array, 0, array.length - 1)
+  }
+  
+  function constructMinHeightBst(array, startIdx, endIdx) {
+      if (startIdx > endIdx) return null;
+      const midIdx = Math.floor((startIdx + endIdx)/2);
+      const bst = new BST(array[midIdx]);
+      bst.left = constructMinHeightBst(array, startIdx, midIdx - 1);
+      bst.right = constructMinHeightBst(array, midIdx + 1, endIdx);
+      return bst
+  }
+  
+  class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  
+    insert(value) {
+      if (value < this.value) {
+        if (this.left === null) {
+          this.left = new BST(value);
+        } else {
+          this.left.insert(value);
+        }
+      } else {
+        if (this.right === null) {
+          this.right = new BST(value);
+        } else {
+          this.right.insert(value);
+        }
+      }
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.minHeightBst = minHeightBst;
