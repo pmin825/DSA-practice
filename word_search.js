e20e6e0b5affaa4a802bdef77d00700825758711
pmin825 <pmin.0825@gmail.com>
2021-01-19 19:17:02 -0800

    update code styling

--- word_search.js
+++ word_search.js
@@ -3,27 +3,38 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {  
+var exist = function (board, word) {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
-            if (existHelper(board, word, 0, i, j)) return true;    
+            if (existHelper(board, word, 0, i, j)) return true;
         }
     }
     return false;
 };
 
-var existHelper = function(board, word, wordIdx, i, j) {
-    if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] !== word[wordIdx]) return false;
+var existHelper = function (board, word, wordIdx, i, j) {
+    if (
+        i < 0 ||
+        i >= board.length ||
+        j < 0 ||
+        j >= board[0].length ||
+        board[i][j] !== word[wordIdx]
+    )
+        return false;
     if (wordIdx === word.length - 1) return true;
-    
+
     let temp = board[i][j];
-    board[i][j] = 'visited'
-    
-    let check = (existHelper(board, word, wordIdx + 1, i - 1, j) || existHelper(board, word, wordIdx + 1, i + 1, j) || existHelper(board, word, wordIdx + 1, i, j - 1) || existHelper(board, word, wordIdx + 1, i, j + 1)) 
+    board[i][j] = "visited";
+
+    let check =
+        existHelper(board, word, wordIdx + 1, i - 1, j) ||
+        existHelper(board, word, wordIdx + 1, i + 1, j) ||
+        existHelper(board, word, wordIdx + 1, i, j - 1) ||
+        existHelper(board, word, wordIdx + 1, i, j + 1);
     if (check) {
         return true;
-    }   else {
+    } else {
         board[i][j] = temp;
         return false;
     }
-}
+};
