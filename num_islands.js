ad28c6daeafca86e402d01fe181f81be7575b17e
pmin825 <pmin.0825@gmail.com>
2021-01-23 13:33:20 -0800

    num islands

---
+++ num_islands.js
@@ -0,0 +1,27 @@
+const numIslands = grid => {
+  let count = 0;    
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+          if (grid[i][j] === '1') {
+              count++
+              traverseGrid(grid, i, j)
+          }
+      }
+  }
+  return count
+}
+const traverseGrid = (grid, i, j) => {
+  if (
+      i < 0 || 
+      i >= grid.length || 
+      j < 0 || 
+      j >= grid[0].length
+  )
+      return 
+  if (grid[i][j] === '0') return;
+  grid[i][j] = '0'
+  traverseGrid(grid, i - 1, j)
+  traverseGrid(grid, i + 1, j)
+  traverseGrid(grid, i, j - 1)
+  traverseGrid(grid, i, j + 1)
+}
