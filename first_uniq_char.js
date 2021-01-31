676478781544a158432ce91e0cfe2865940ab86b
pmin825 <pmin.0825@gmail.com>
2021-01-31 15:43:39 -0800

    first uniq char

---
+++ first_uniq_char.js
@@ -0,0 +1,18 @@
+const firstUniqChar = (s) => {
+  let count = {};
+
+  for (const char of s) {
+    if (count[char]) {
+      count[char] += 1;
+    } else {
+      count[char] = 1;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (count[char] > 1) continue;
+    return i;
+  }
+  return -1;
+};
