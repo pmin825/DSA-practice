e2ee17ac2178f965c0c6e5daf2a1e1ad3c81a7c9
pmin825 <pmin.0825@gmail.com>
2021-02-04 15:51:56 -0800

    merge intervals alternate solution

--- merge_intervals.js
+++ merge_intervals.js
@@ -1,18 +1,48 @@
+// const merge = (intervals) => {
+//     intervals.sort((a, b) => a[0] - b[0]);
+//     let i = 0;
+//     while (i < intervals.length - 1) {
+//         let current = intervals[i];
+//         let next = intervals[i + 1];
+//         let newInt = [];
+
+//         if (current[1] >= next[0]) {
+//             newInt[0] = Math.min(current[0], next[0]);
+//             newInt[1] = Math.max(current[1], next[1]);
+//             intervals.splice(i, 2, newInt);
+//         } else {
+//             i++;
+//         }
+//     }
+//     return intervals;
+// };
+
 const merge = (intervals) => {
     intervals.sort((a, b) => a[0] - b[0]);
-    let i = 0;
-    while (i < intervals.length) {
-        let current = intervals[i];
-        let next = intervals[i + 1];
-        let newInt = [];
+    let results = [];
+    let start = intervals[0][0];
+    let end = intervals[0][1];
 
-        if (next && current[1] >= next[0]) {
-            newInt[0] = Math.min(current[0], next[0]);
-            newInt[1] = Math.max(current[1], next[1]);
-            intervals.splice(i, 2, newInt);
-        } else {
-            i++;
+    for (let i = 0; i < intervals.length; i++) {
+        let interval = intervals[i];
+        let checkStart = interval[0];
+        let checkEnd = interval[1];
+
+        if (checkStart <= end) {
+            if (checkEnd > end) {
+                end = checkEnd;
+            }
+        }
+
+        if (checkStart > end) {
+            results.push([start, end]);
+            start = checkStart;
+            end = checkEnd;
+        }
+
+        if (i === intervals.length - 1) {
+            results.push([start, end]);
         }
     }
-    return intervals;
+    return results;
 };
