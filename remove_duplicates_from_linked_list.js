b636409a184fff3cc51d87594be1ebe94a977996
pmin825 <pmin.0825@gmail.com>
2021-02-08 17:10:41 -0800

    remove duplicates from linked list

---
+++ remove_duplicates_from_linked_list.js
@@ -0,0 +1,27 @@
+// This is an input class. Do not edit.
+class LinkedList {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  
+  function removeDuplicatesFromLinkedList(linkedList) {
+      let node = linkedList;
+      
+      while (node !== null) {
+          let nextDistinctNode = node.next;
+          while (nextDistinctNode !== null && nextDistinctNode.value === node.value) {
+              nextDistinctNode = nextDistinctNode.next;
+          }
+          node.next = nextDistinctNode;
+          node = nextDistinctNode;
+      }
+      return linkedList
+  }
+  
+  
+  
+  // Do not edit the lines below.
+  exports.LinkedList = LinkedList;
+  exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
