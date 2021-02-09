6f3601cc769c5da0d413b14d9b50549c1005f042
pmin825 <pmin.0825@gmail.com>
2021-02-08 20:24:28 -0800

    tournament winner

---
+++ tournament_winner.js
@@ -0,0 +1,23 @@
+function tournamentWinner(competitions, results) {
+	let scoreMap = {};
+  for (let i = 0; i < competitions.length; i++) {
+		let comp = competitions[i];
+		if (results[i] === 0) {
+			if (scoreMap[comp[1]]) {
+				scoreMap[comp[1]] += 3;
+			}	else {
+				scoreMap[comp[1]] = 3;
+			}
+		}
+		if (results[i] === 1) {
+			if (scoreMap[comp[0]]) {
+				scoreMap[comp[0]] += 3;
+			}	else {
+				scoreMap[comp[0]] = 3;
+			}
+		}
+	}
+
+	let scoreArr = Object.entries(scoreMap).sort((a,b) => b[1] - a[1]);
+	return scoreArr[0][0];
+}
