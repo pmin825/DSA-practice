ac5fc904f22fc54d3d1bf8e91ef1812c16d98a98
pmin825 <pmin.0825@gmail.com>
2021-02-11 13:11:45 -0800

    staircase traversal using sliding window

--- staircase_traversal.js
+++ staircase_traversal.js
@@ -1,14 +1,29 @@
-function staircaseTraversal(height, maxSteps) {
-    let numWays = new Array(height + 1).fill(0);
-    numWays[0] = 1;
-    numWays[1] = 1;
+// function staircaseTraversal(height, maxSteps) {
+//     let numWays = new Array(height + 1).fill(0);
+//     numWays[0] = 1;
+//     numWays[1] = 1;
 
-    for (let i = 2; i <= height; i++) {
-        let step = 1;
-        while (step <= maxSteps && step <= i) {
-            numWays[i] += numWays[i - step];
-            step += 1;
-        }
+//     for (let i = 2; i <= height; i++) {
+//         let step = 1;
+//         while (step <= maxSteps && step <= i) {
+//             numWays[i] += numWays[i - step];
+//             step += 1;
+//         }
+//     }
+//     return numWays[height];
+// }
+
+function staircaseTraversal(height, maxSteps) {
+    let count = 0;
+    let numWays = [1];
+
+    for (let i = 1; i <= height; i++) {
+        let start = i - maxSteps - 1;
+        let end = i - 1;
+
+        if (start >= 0) count -= numWays[start];
+        count += numWays[end];
+        numWays.push(count);
     }
     return numWays[height];
 }
