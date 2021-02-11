1001eb81cda53c90ba057fc59d8812491d413130
pmin825 <pmin.0825@gmail.com>
2021-02-11 12:19:12 -0800

    staircase traversal, DP

--- remove_duplicates_from_linked_list.js
+++ remove_duplicates_from_linked_list.js
@@ -1,27 +1,28 @@
 // This is an input class. Do not edit.
 class LinkedList {
     constructor(value) {
-      this.value = value;
-      this.next = null;
+        this.value = value;
+        this.next = null;
     }
-  }
-  
-  function removeDuplicatesFromLinkedList(linkedList) {
-      let node = linkedList;
-      
-      while (node !== null) {
-          let nextDistinctNode = node.next;
-          while (nextDistinctNode !== null && nextDistinctNode.value === node.value) {
-              nextDistinctNode = nextDistinctNode.next;
-          }
-          node.next = nextDistinctNode;
-          node = nextDistinctNode;
-      }
-      return linkedList
-  }
-  
-  
-  
-  // Do not edit the lines below.
-  exports.LinkedList = LinkedList;
-  exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+    let node = linkedList;
+
+    while (node !== null) {
+        let nextDistinctNode = node.next;
+        while (
+            nextDistinctNode !== null &&
+            nextDistinctNode.value === node.value
+        ) {
+            nextDistinctNode = nextDistinctNode.next;
+        }
+        node.next = nextDistinctNode;
+        node = nextDistinctNode;
+    }
+    return linkedList;
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
---
+++ staircase_traversal.js
@@ -0,0 +1,14 @@
+function staircaseTraversal(height, maxSteps) {
+    let numWays = new Array(height + 1).fill(0);
+    numWays[0] = 1;
+    numWays[1] = 1;
+
+    for (let i = 2; i <= height; i++) {
+        let step = 1;
+        while (step <= maxSteps && step <= i) {
+            numWays[i] = numWays[i] + numWays[i - step];
+            step += 1;
+        }
+    }
+    return numWays[height];
+}
