5c8732bddc2ce8c677fef534e2c63e85fe0791e9
pmin825 <pmin.0825@gmail.com>
2021-02-12 17:09:22 -0800

    single cycle check

--- single_cycle_check.js
+++ single_cycle_check.js
@@ -1,18 +1,19 @@
 function hasSingleCycle(array) {
-	let idx = 0;
-	let count = 0;
-  	while (count < array.length) {
-		if (count > 0 && idx === 0) return false;
-		count++
-		idx = getNextIdx(idx, array);
-	}
-	return idx === 0;
+    let visited = 0;
+    let idx = 0;
+
+    while (visited < array.length) {
+        if (visited > 0 && idx === 0) return false;
+        visited++;
+        idx = getNextIdx(idx, array);
+    }
+    return idx === 0;
 }
 
 function getNextIdx(idx, array) {
-	const jump = array[idx];
-	const nextIdx = (idx + jump) % array.length;
-	return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+    const jump = array[idx];
+    const nextIdx = (idx + jump) % array.length;
+    return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
 }
 
 // Do not edit the line below.
