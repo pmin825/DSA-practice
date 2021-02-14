c2aa3b2b19726b5e2b890f77a7c52493289020f2
pmin825 <pmin.0825@gmail.com>
2021-02-13 20:29:46 -0800

    branch sums helper

--- branch_sums.js
+++ branch_sums.js
@@ -1,34 +1,25 @@
-// This is the class of the input root.
-// Do not edit it.
-// Algo Expert - Branch Sums 
-
 class BinaryTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 function branchSums(root) {
-	let sums = [];
-	branchSumsHelper(root, 0, sums)
-	return sums;
+    let sums = [];
+    branchSumsHelper(root, 0, sums);
+    return sums;
 }
 
 function branchSumsHelper(node, runningSum, sums) {
-	if (!node) return;
-	let newRunningSum = runningSum + node.value;
-	if (!node.left && !node.right) {
-		sums.push(newRunningSum);
-		return;
-	}
-	
-	branchSumsHelper(node.left, newRunningSum, sums);
-	branchSumsHelper(node.right, newRunningSum, sums);
+    if (!node) return;
+
+    const newRunningSum = runningSum + node.value;
+    if (!node.left && !node.right) {
+        sums.push(newRunningSum);
+        return;
+    }
+    branchSumsHelper(node.left, newRunningSum, sums);
+    branchSumsHelper(node.right, newRunningSum, sums);
 }
-
-// Do not edit the lines below.
-exports.BinaryTree = BinaryTree;
-exports.branchSums = branchSums;
-
