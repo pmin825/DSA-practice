10d26fc151d394133aa7d7abf6f57594254c852c
pmin825 <pmin.0825@gmail.com>
2021-02-13 19:23:01 -0800

    subsets

---
+++ subsets.js
@@ -0,0 +1,10 @@
+const subsets = (nums) => {
+    let subs = [[]];
+    for (const num of nums) {
+        let length = subs.length;
+        for (let i = 0; i < length; i++) {
+            subs.push(subs[i].concat(num));
+        }
+    }
+    return subs;
+};
