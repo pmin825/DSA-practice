ae4002b0dd0fd24323b76e64e88325c1f06c3838
pmin825 <pmin.0825@gmail.com>
2021-02-14 11:19:29 -0800

    node depths

--- node_depths.js
+++ node_depths.js
@@ -1,6 +1,8 @@
-// Algo-Expert: Node Depths
-
 function nodeDepths(root, depth = 0) {
-	if (root === null) return 0;
-	return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1);
+    if (root === null) return 0;
+    return (
+        depth +
+        nodeDepths(root.left, depth + 1) +
+        nodeDepths(root.right, depth + 1)
+    );
 }
