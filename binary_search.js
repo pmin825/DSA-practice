b4a1fe3d536cd33b31801e0c93817d319d76260c
peter min <peter@peters-MacBook-Pro.local>
2021-02-19 11:55:52 -0800

    binary search

--- binary_search.js
+++ binary_search.js
@@ -1,17 +1,18 @@
 // O(log(n)) time | O(log(1)) space
 
 function binarySearch(array, target) {
-    let left = 0;
-	let right = array.length - 1;
-	while (left <= right) {
-		let mid = Math.floor((left + right)/2);
-		if (target === array[mid]) {
-			return mid;
-		}	else if (target < array[mid]) {
-			right = mid - 1;
-		}	else {
-			left = mid + 1;
-		}
-	}
-	return -1
+    return bSearchHelper(array, target, 0, array.length - 1);
+}
+
+function bSearchHelper(array, target, left, right) {
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (array[mid] === target) return mid;
+        if (target > array[mid]) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
 }
