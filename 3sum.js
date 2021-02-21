ea3d4c18571e2081a76a84871daf149ecf1dd389
peter min <peter@peters-MacBook-Pro.local>
2021-02-21 11:49:42 -0800

    3sum

---
+++ 3sum.js
@@ -0,0 +1,27 @@
+const threeSum = (array) => {
+    const results = [];
+    array.sort((a, b) => a - b);
+
+    for (let i = 0; i < array.length; i++) {
+        let left = i + 1;
+        let right = array.length - 1;
+
+        while (left < right) {
+            let sum = array[i] + array[left] + array[right];
+            if (sum === 0) {
+                results.push([array[i], array[left], array[right]]);
+                while (array[left + 1] === array[left]) left++;
+                while (array[right - 1] === array[right]) right--;
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        while (array[i + 1] === array[i]) i++;
+    }
+    return results;
+};
