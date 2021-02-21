3eee4509b4dea0875f0c937e0b3d11849d75f1ed
peter min <peter@peters-MacBook-Pro.local>
2021-02-20 20:03:39 -0800

    validate bst

--- validate_bst.js
+++ validate_bst.js
@@ -1,24 +1,22 @@
-// This is an input class. Do not edit.
 class BST {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 function validateBst(tree) {
-	return validateBstHelper(tree, -Infinity, Infinity);
+    return validateBstHelper(tree, -Infinity, Infinity);
 }
 
 function validateBstHelper(tree, min, max) {
-	if (tree === null) return true;
-	if (tree.value >= max || tree.value < min) return false;
-	
-	return validateBstHelper(tree.left, min, tree.value) 
-		&& validateBstHelper(tree.right, tree.value, max)
-}
+    if (tree === null) return true;
+    if (tree.value >= max) return false;
+    if (tree.value < min) return false;
 
-// Do not edit the line below.
-exports.BST = BST;
-exports.validateBst = validateBst;
+    return (
+        validateBstHelper(tree.left, min, tree.value) &&
+        validateBstHelper(tree.right, tree.value, max)
+    );
+}
