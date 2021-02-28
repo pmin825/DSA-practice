65aa2194dd259afae48ed764c349875ee4df4ca4
pmin825 <pmin.0825@gmail.com>
2021-02-28 13:31:21 -0800

    find kth largest value in bst

---
+++ find_kth_largest_value_in_bst.js
@@ -0,0 +1,21 @@
+class BST {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function findKthLargestValueInBst(tree, k) {
+    const sortedNodeValues = [];
+    inOrderTraverse(tree, sortedNodeValues);
+    return sortedNodeValues[sortedNodeValues.length - k];
+}
+
+function inOrderTraverse(node, sortedNodeValues) {
+    if (node === null) return;
+
+    inOrderTraverse(node.left, sortedNodeValues);
+    sortedNodeValues.push(node.value);
+    inOrderTraverse(node.right, sortedNodeValues);
+}
