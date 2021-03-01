8a096fc2c2de536d4b896d3eaf7888bb368e2f95
pmin825 <pmin.0825@gmail.com>
2021-02-28 19:59:57 -0800

    number of ways to traverse graph dp

--- number_of_ways_to_traverse_graph.js
+++ number_of_ways_to_traverse_graph.js
@@ -1,8 +1,31 @@
-function numberOfWaysToTraverseGraph(width, height) {
-    if (width === 1 || height === 1) return 1;
+// function numberOfWaysToTraverseGraph(width, height) {
+//     if (width === 1 || height === 1) return 1;
 
-    return (
-        numberOfWaysToTraverseGraph(width - 1, height) +
-        numberOfWaysToTraverseGraph(width, height - 1)
-    );
+//     return (
+//         numberOfWaysToTraverseGraph(width - 1, height) +
+//         numberOfWaysToTraverseGraph(width, height - 1)
+//     );
+// }
+
+function numberOfWaysToTraverseGraph(width, height) {
+    const numWays = [];
+    for (let i = 0; i < height + 1; i++) {
+        numWays.push([]);
+        for (let j = 0; j < width + 1; j++) {
+            numWays[i].push(0);
+        }
+    }
+
+    for (let i = 1; i < width + 1; i++) {
+        for (let j = 1; j < height + 1; j++) {
+            if (i === 1 || j === 1) {
+                numWays[j][i] = 1;
+            } else {
+                const waysLeft = numWays[j][i - 1];
+                const waysUp = numWays[j - 1][i];
+                numWays[j][i] = waysLeft + waysUp;
+            }
+        }
+    }
+    return numWays[height][width];
 }
