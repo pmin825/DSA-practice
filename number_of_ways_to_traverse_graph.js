9bf973d498c39cb1910619bf38d276617e80b9e3
pmin825 <pmin.0825@gmail.com>
2021-02-28 19:23:29 -0800

    number of ways to traverse graph

---
+++ number_of_ways_to_traverse_graph.js
@@ -0,0 +1,8 @@
+function numberOfWaysToTraverseGraph(width, height) {
+    if (width === 1 || height === 1) return 1;
+
+    return (
+        numberOfWaysToTraverseGraph(width - 1, height) +
+        numberOfWaysToTraverseGraph(width, height - 1)
+    );
+}
