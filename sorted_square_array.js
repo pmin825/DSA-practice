c071ce8f0203f8e5a432c8667b56d46f76fd22eb
pmin825 <pmin.0825@gmail.com>
2021-03-07 15:22:27 -0800

    sorted square array alt solution

--- sorted_square_array.js
+++ sorted_square_array.js
@@ -7,3 +7,21 @@ function sortedSquaredArray(array) {
   }
   return results.sort((a, b) => a - b);
 }
+
+//O(n) time O(n) space solution
+function sortedSquaredArray(array) {
+  const results = new Array(array.length).fill(0);
+  let left = 0;
+  let right = array.length - 1;
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (Math.abs(array[left]) > Math.abs(array[right])) {
+      results[i] = Math.pow(array[left], 2);
+      left++;
+    } else {
+      results[i] = Math.pow(array[right], 2);
+      right--;
+    }
+  }
+  return results;
+}
