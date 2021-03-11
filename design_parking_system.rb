12b71dbdc14ea0a210bcb033cb97d21c64bf0b26
pmin825 <pmin825@gmail.com>
2021-03-10 18:08:35 -0800

    design parking system

---
+++ design_parking_system.rb
@@ -0,0 +1,22 @@
+class ParkingSystem
+
+  =begin
+      :type big: Integer
+      :type medium: Integer
+      :type small: Integer
+  =end
+      def initialize(big, medium, small)
+          @spaces = [0, big, medium, small]
+      end
+  
+  
+  =begin
+      :type car_type: Integer
+      :rtype: Boolean
+  =end
+      def add_car(car_type)
+          return false if 0 == @spaces[car_type]
+          @spaces[car_type] -= 1
+          true
+      end
+  end
