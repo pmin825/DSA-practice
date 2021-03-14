4597e5025f3d72aba8284b1e1fdd6083b2e01b73
pmin825 <pmin.0825@gmail.com>
2021-03-14 12:48:17 -0700

    find min in rotated sorted arr

--- find_min_in_rotated_sorted_array.js
+++ find_min_in_rotated_sorted_array.js
@@ -1,8 +1,25 @@
-const findMin = (nums) => {
-    let smallest = nums[0];
+// const findMin = (nums) => {
+//     let smallest = nums[0];
 
-    for (let i = 1; i < nums.length; i++) {
-        smallest = Math.min(smallest, nums[i]);
+//     for (let i = 1; i < nums.length; i++) {
+//         smallest = Math.min(smallest, nums[i]);
+//     }
+//     return smallest;
+// };
+
+
+//binary search approach
+const findMin = (nums) => {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        }   else {
+            right = mid;
+        }
     }
-    return smallest;
-};
+    return nums[left]
+}
