e48115b15116d760b34974fc773d5227256c3e24
pmin825 <pmin.0825@gmail.com>
2021-03-15 14:17:17 -0700

    invert binary tree

--- invert_binary_tree.js
+++ invert_binary_tree.js
@@ -1,14 +1,23 @@
 // O(n) time | O(n) space
 
 function invertBinaryTree(tree) {
-	let queue = [tree];
-	while (queue.length) {
-		let node = queue.shift();
-		if (node === null) continue;
-		let temp = node.left;
-		node.left = node.right;
-		node.right = temp;
-		queue.push(node.left, node.right)
-	}
-	return tree;
+    let queue = [tree];
+    while (queue.length) {
+        let node = queue.shift();
+        if (node === null) continue;
+        let temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        queue.push(node.left, node.right);
+    }
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
