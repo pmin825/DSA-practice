e9afe18da6b869df0dc7d6afa636a7abcb3d7f25
pmin825 <pmin.0825@gmail.com>
2021-03-16 13:47:48 -0700

    set matrix zeroes

--- set_matrix_zeroes.js
+++ set_matrix_zeroes.js
@@ -1,21 +1,23 @@
 const setZeroes = (matrix) => {
-    let col0 = 1,
-        row = matrix.length,
-        col = matrix[0].length;
-
-    for (let i = 0; i < row; i++) {
-        if (matrix[i][0] === 0) col0 = 0;
-        for (let j = 1; j < col; j++) {
+    let col0 = false;
+    
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] === 0) col0 = true;
+        for (let j = 1; j < matrix[0].length; j++) {
             if (matrix[i][j] === 0) {
-                matrix[i][0] = matrix[0][j] = 0;
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
             }
         }
     }
-
-    for (let i = row - 1; i >= 0; i--) {
-        for (let j = col - 1; j > 0; j--) {
-            if (matrix[i][0] === 0 || matrix[0][j] === 0) matrix[i][j] = 0;
+    
+    for (let i = matrix.length - 1; i >= 0; i--) {
+        for (let j = matrix[0].length - 1; j >= 1; j--) {
+            if (matrix[0][j] === 0 || matrix[i][0] === 0) {
+                matrix[i][j] = 0;
+            }
         }
-        if (col0 === 0) matrix[i][0] = 0;
+        if (col0) matrix[i][0] = 0;
     }
+    return matrix;
 };
