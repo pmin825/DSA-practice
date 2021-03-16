e94fe5e54de4eb3c493a2fead050b6a186ac3293
pmin825 <pmin.0825@gmail.com>
2021-03-15 17:27:51 -0700

    product sums

--- product_sums.js
+++ product_sums.js
@@ -1,12 +1,13 @@
-function productSum(array, totalDepth = 1) {
-	let totalSum = 0; 
-	for (let i = 0; i < array.length; i++) { 
-		let el = array[i]; 
-		if (el instanceof Array) {
-			totalSum += productSum(el, totalDepth + 1) 
-		}	else {
-			totalSum += el; 
-		}
-	}
-	return totalSum * totalDepth;
+function productSum(array, mult = 1) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        let ele = array[i];
+
+        if (ele instanceof Array) {
+            total += productSum(ele, mult + 1);
+        } else {
+            total += ele;
+        }
+    }
+    return total * mult;
 }
