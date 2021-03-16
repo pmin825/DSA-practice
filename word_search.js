270aa735a70e4d58b104644512ba25308e120e4b
pmin825 <pmin.0825@gmail.com>
2021-03-15 19:16:43 -0700

    word search

--- word_search.js
+++ word_search.js
@@ -3,38 +3,41 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function (board, word) {
+const exist = (board, word) => {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
-            if (existHelper(board, word, 0, i, j)) return true;
+            if (wordFinder(board, i, j, word, 0)) {
+                return true;
+            }
         }
     }
     return false;
 };
 
-var existHelper = function (board, word, wordIdx, i, j) {
+const wordFinder = (board, i, j, word, charCount) => {
     if (
         i < 0 ||
-        i >= board.length ||
         j < 0 ||
+        i >= board.length ||
         j >= board[0].length ||
-        board[i][j] !== word[wordIdx]
+        board[i][j] !== word[charCount]
     )
         return false;
-    if (wordIdx === word.length - 1) return true;
+
+    if (charCount === word.length - 1) return true;
 
     let temp = board[i][j];
     board[i][j] = "visited";
 
     let check =
-        existHelper(board, word, wordIdx + 1, i - 1, j) ||
-        existHelper(board, word, wordIdx + 1, i + 1, j) ||
-        existHelper(board, word, wordIdx + 1, i, j - 1) ||
-        existHelper(board, word, wordIdx + 1, i, j + 1);
+        wordFinder(board, i - 1, j, word, charCount + 1) ||
+        wordFinder(board, i + 1, j, word, charCount + 1) ||
+        wordFinder(board, i, j - 1, word, charCount + 1) ||
+        wordFinder(board, i, j + 1, word, charCount + 1);
+
     if (check) {
         return true;
     } else {
         board[i][j] = temp;
-        return false;
     }
 };
