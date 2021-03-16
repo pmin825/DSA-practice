65d9281b982a476cb6aeb465c815353997540465
pmin825 <pmin.0825@gmail.com>
2021-03-15 17:58:58 -0700

    binary search

--- binary_search.js
+++ binary_search.js
@@ -1,17 +1,16 @@
 // O(log(n)) time | O(log(1)) space
 
 function binarySearch(array, target) {
-    return bSearchHelper(array, target, 0, array.length - 1);
-}
+    let left = 0;
+    let right = array.length - 1;
 
-function bSearchHelper(array, target, left, right) {
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
         if (array[mid] === target) return mid;
-        if (target > array[mid]) {
-            left = mid + 1;
-        } else {
+        if (array[mid] > target) {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
     }
     return -1;
