54fa08072696a4d8de777c69d86d728023790d8f
pmin825 <pmin.0825@gmail.com>
2021-03-18 11:45:04 -0700

    run length encoding

--- run_length_encoding.js
+++ run_length_encoding.js
@@ -1,16 +1,18 @@
 function runLengthEncoding(string) {
-  let counter = 1;
-	let encoded = "";
-	for (let i = 0; i < string.length; i++){
-		if (string[i] === string[i + 1] && counter < 9){
-			counter++;
-		} else if (string[i] === string[i + 1] && counter === 9){
-			encoded += counter.toString() + string[i].toString()
-			counter = 1
-		} else if (string[i] !== string[i + 1]){
-			encoded += counter.toString() + string[i].toString()
-			counter = 1
-		}
-	}
-	return encoded;
+    let final = "";
+    let count = 1;
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i + 1]) {
+            count += 1;
+            if (count >= 10) {
+                count = (count % 10) + 1;
+                final += "9" + string[i];
+            }
+        } else {
+            final += count.toString() + string[i];
+            count = 1;
+        }
+    }
+    return final;
 }
