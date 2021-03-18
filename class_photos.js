8be3cc93c4d0eb254396096728df7b299e0e9ad4
pmin825 <pmin.0825@gmail.com>
2021-03-18 12:04:30 -0700

    class photos

--- class_photos.js
+++ class_photos.js
@@ -2,21 +2,13 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
     redShirtHeights.sort((a, b) => a - b);
     blueShirtHeights.sort((a, b) => a - b);
 
-    if (redShirtHeights[0] > blueShirtHeights[0]) {
-        for (let i = 0; i < redShirtHeights.length; i++) {
-            let redNum = redShirtHeights[i];
-
-            if (redNum <= blueShirtHeights[i]) return false;
+    for (let i = 0; i < redShirtHeights.length; i++) {
+        if (redShirtHeights[i] === blueShirtHeights[i]) return false;
+        if (redShirtHeights[0] > blueShirtHeights[0]) {
+            if (redShirtHeights[i] <= blueShirtHeights[i]) return false;
+        } else {
+            if (blueShirtHeights[i] <= redShirtHeights[i]) return false;
         }
-        return true;
-    } else if (blueShirtHeights[0] > redShirtHeights[0]) {
-        for (let i = 0; i < blueShirtHeights.length; i++) {
-            let blueNum = blueShirtHeights[i];
-
-            if (blueNum <= redShirtHeights[i]) return false;
-        }
-        return true;
-    } else {
-        return false;
     }
+    return true;
 }
