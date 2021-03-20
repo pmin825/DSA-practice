1275ab8917aecb0ce037be67feebc973a0ac0b73
pmin825 <pmin.0825@gmail.com>
2021-03-20 12:46:06 -0700

    remove duplicate letters

---
+++ suffix_trie.js
@@ -0,0 +1,32 @@
+class SuffixTrie {
+    constructor(string) {
+        this.root = {};
+        this.endSymbol = "*";
+        this.populateSuffixTrieFrom(string);
+    }
+
+    populateSuffixTrieFrom(string) {
+        for (let i = 0; i < string.length; i++) {
+            this.insertSubstringAt(i, string);
+        }
+    }
+
+    insertSubstringAt(i, string) {
+        let node = this.root;
+        for (let j = i; j < string.length; j++) {
+            const char = string[j];
+            if (!node[char]) node[char] = {};
+            node = node[char];
+        }
+        node[this.endSymbol] = true;
+    }
+
+    contains(string) {
+        let node = this.root;
+        for (const char of string) {
+            if (!node[char]) return false;
+            node = node[char];
+        }
+        return !!node[this.endSymbol];
+    }
+}
