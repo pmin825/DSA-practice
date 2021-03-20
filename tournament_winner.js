d0148874300c979b878a13bae2a28ab8ce6a8696
pmin825 <pmin.0825@gmail.com>
2021-03-19 17:06:22 -0700

    tournament winner

--- tournament_winner.js
+++ tournament_winner.js
@@ -1,23 +1,33 @@
 function tournamentWinner(competitions, results) {
-    let scoreMap = {};
+    const scores = {};
     for (let i = 0; i < competitions.length; i++) {
+        let winner = results[i];
         let comp = competitions[i];
-        if (results[i] === 0) {
-            if (scoreMap[comp[1]]) {
-                scoreMap[comp[1]] += 3;
+        let home = comp[0];
+        let away = comp[1];
+
+        if (winner === 1) {
+            if (!scores[home]) {
+                scores[home] = 3;
             } else {
-                scoreMap[comp[1]] = 3;
+                scores[home] += 3;
             }
-        }
-        if (results[i] === 1) {
-            if (scoreMap[comp[0]]) {
-                scoreMap[comp[0]] += 3;
+        } else {
+            if (!scores[away]) {
+                scores[away] = 3;
             } else {
-                scoreMap[comp[0]] = 3;
+                scores[away] += 3;
             }
         }
     }
 
-    let scoreArr = Object.entries(scoreMap).sort((a, b) => b[1] - a[1]);
-    return scoreArr[0][0];
+    let highScore;
+    let max = 0;
+    for (const [key, value] of Object.entries(scores)) {
+        if (value > max) {
+            max = value;
+            highScore = key;
+        }
+    }
+    return highScore;
 }
