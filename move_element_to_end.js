a17305932713f10def82c72d9e50da47aecd0d40
pmin825 <pmin.0825@gmail.com>
2021-03-20 17:30:29 -0700

    move element to end

--- move_element_to_end.js
+++ move_element_to_end.js
@@ -1,31 +1,14 @@
 function moveElementToEnd(array, toMove) {
-	for (let i = array.length - 1; i >= 0; i--) {
-		let num = array[i];
-		if (num === toMove) {
-			array.splice(i, 1);
-			array.push(toMove);
-		}
-	}
-	return array;
+    let left = 0;
+    let right = array.length - 1;
+
+    while (left < right) {
+        if (array[left] === toMove) {
+            [array[left], array[right]] = [array[right], array[left]];
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return array;
 }
-
-
-// O(n) time | O(1) space
-function moveElementToEnd(array, toMove) {
-	let start = 0;
-	let end = array.length - 1;
-	
-	while (start < end) {
-		while (start < end && array[end] === toMove) end--;
-		if (array[start] === toMove) {
-			let temp = array[end];
-			array[end] = array[start];
-			array[start] = temp;
-		}
-		start++
-	}
-	return array;
-}
-
-// Do not edit the line below.
-exports.moveElementToEnd = moveElementToEnd;
