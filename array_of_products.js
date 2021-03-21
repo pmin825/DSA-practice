bdc0dd080b4ad8a2db6de4e8421362681de689f7
pmin825 <pmin.0825@gmail.com>
2021-03-20 18:36:51 -0700

    array of products

--- array_of_products.js
+++ array_of_products.js
@@ -1,12 +1,29 @@
+// function arrayOfProducts(array) {
+//     const results = [];
+//     for (let i = 0; i < array.length; i++) {
+//         let product = 1;
+//         for (let j = 0; j < array.length; j++) {
+//             if (j === i) continue;
+//             product *= array[j];
+//         }
+//         results.push(product);
+//     }
+//     return results;
+// }
+
+// O(n) time O(n) space
 function arrayOfProducts(array) {
-    const results = [];
+    let results = new Array(array.length).fill(1);
+
+    let leftProd = 1;
     for (let i = 0; i < array.length; i++) {
-        let product = 1;
-        for (let j = 0; j < array.length; j++) {
-            if (j === i) continue;
-            product *= array[j];
-        }
-        results.push(product);
+        results[i] = leftProd;
+        leftProd *= array[i];
+    }
+    let rightProd = 1;
+    for (let i = array.length - 1; i >= 0; i--) {
+        results[i] *= rightProd;
+        rightProd *= array[i];
     }
     return results;
 }
