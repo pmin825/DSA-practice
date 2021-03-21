0df65e2e2467149487ce297447621c5b89c48caf
pmin825 <pmin.0825@gmail.com>
2021-03-20 17:09:32 -0700

    smallest difference

--- smallest_difference.js
+++ smallest_difference.js
@@ -1,26 +1,23 @@
-// O(nlog(n)) time | O(1) space 
-
 function smallestDifference(arrayOne, arrayTwo) {
-    let idxOne = 0;
-    let idxTwo = 0;
-    let smallest = Infinity;
-    let final;
-    while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) {
-        let numOne = arrayOne[idxOne];
-        let numTwo = arrayTwo[idxTwo];
-        let current = Math.abs(numOne - numTwo)
-        idxTwo++
-        if (current < smallest) {
-            smallest = current;
-            final = [numOne, numTwo]
-        }
-        if (idxTwo === arrayTwo.length) {
-            idxTwo = 0
-            idxOne++
-        }
-    }
-    return final
+	let min = Infinity;
+	let result = [];
+	
+	for (let i = 0; i < arrayOne.length; i++) {
+		let diff;
+		for (let j = 0; j < arrayTwo.length; j++) {
+			if (
+					arrayOne[i] >= 0 && arrayTwo[j] >= 0 ||
+					arrayOne[i] < 0 && arrayTwo[j] < 0
+			)	{
+				diff = Math.abs(arrayOne[i] - arrayTwo[j])
+			}	else {
+				diff = Math.abs(arrayOne[i]) + Math.abs(arrayTwo[i]);
+			}
+			if (diff < min) {
+				min = diff;
+				result = [arrayOne[i], arrayTwo[j]];
+			}
+		}
+	}	
+	return result;
 }
-// Do not edit the line below.
-exports.smallestDifference = smallestDifference;
-
