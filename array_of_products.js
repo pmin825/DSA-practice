19ad8ea476953e6bb045bd840160b51cf6759403
pmin825 <pmin.0825@gmail.com>
2021-03-20 18:27:02 -0700

    array of products

--- array_of_products.js
+++ array_of_products.js
@@ -1,41 +1,12 @@
-// O(n) time | O(n) space
 function arrayOfProducts(array) {
-	const products = new Array(array.length).fill(1); // [8, 40, 10, 20]
-	
-	let leftRunningProduct = 1; // 20 
-	for (let i = 0; i < array.length; i++) { // 3
-		products[i] = leftRunningProduct; // products[3] = 20
-		leftRunningProduct *= array[i]; // 40
-	}
-	
-	let rightRunningProduct = 1; // 40
-	for (let i = array.length - 1; i >= 0; i--) { // 0
-		products[i] *= rightRunningProduct; // 8
-		rightRunningProduct *= array[i]; // 40
-	}
-	return products;
+    const results = [];
+    for (let i = 0; i < array.length; i++) {
+        let product = 1;
+        for (let j = 0; j < array.length; j++) {
+            if (j === i) continue;
+            product *= array[j];
+        }
+        results.push(product);
+    }
+    return results;
 }
-
-// O(n^2) time | O(n) space
-function arrayOfProducts(array) {
-  let output = []; 
-	
-	
-	for (let i = 0; i < array.length; i++) {
-		let multiple = 1;
-		let j = 0;
-		let num = array[j];
-		while (j < array.length) {
-			if (i !== j) {
-				multiple *= array[j];
-			}
-			j++		
-		}
-		
-		output.push(multiple)
-	}
-	return output;
-}
-
-// Do not edit the line below.
-exports.arrayOfProducts = arrayOfProducts;
