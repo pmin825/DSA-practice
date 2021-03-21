ebe00860d057225c9692c6f6fce9706450669a28
pmin825 <pmin.0825@gmail.com>
2021-03-20 18:14:42 -0700

    longest peak

--- longest_peak.js
+++ longest_peak.js
@@ -1,31 +1,23 @@
-// O(n) time | O(1) space
-
 function longestPeak(array) {
-	let longest = 0;
-	let i = 1;
-  while (i < array.length) {
-		let peak = (array[i] > array[i + 1]) && (array[i] > array[i - 1]);
-		if (!peak) {
-			i++
-			continue;
-		}
-		let leftIndex = i - 2;
-		while (leftIndex >= 0 && array[leftIndex] < array[leftIndex + 1]) {
-				leftIndex--
-				console.log(leftIndex)
-		}
-		let rightIndex = i + 2;
-		while (rightIndex < array.length && array[rightIndex] < array[rightIndex - 1]) {
-				rightIndex++
-		}
-		let currentPeak = rightIndex - leftIndex - 1;
-// 		console.log(rightIndex)
-// 		console.log(leftIndex)
-		longest = Math.max(longest, currentPeak);
-		i = rightIndex;
-	}
-	return longest;
-}
+    let longest = 0;
+    let i = 1;
+    while (i < array.length) {
+        let peak = array[i - 1] < array[i] && array[i] > array[i + 1];
+        if (!peak) {
+            i++;
+            continue;
+        }
+        let left = i - 1;
+        let right = i + 1;
 
-// array = [0, 1, 2, 1, 0, 3, 4] 
-// longestPeak(array)
+        while (left - 1 >= 0 && array[left - 1] < array[left]) {
+            left--;
+        }
+        while (right + 1 < array.length && array[right + 1] < array[right]) {
+            right++;
+        }
+        longest = Math.max(longest, right - left + 1);
+        i = right + 1;
+    }
+    return longest;
+}
--- smallest_difference.js
+++ smallest_difference.js
@@ -1,23 +1,23 @@
 function smallestDifference(arrayOne, arrayTwo) {
-	let min = Infinity;
-	let result = [];
-	
-	for (let i = 0; i < arrayOne.length; i++) {
-		let diff;
-		for (let j = 0; j < arrayTwo.length; j++) {
-			if (
-					arrayOne[i] >= 0 && arrayTwo[j] >= 0 ||
-					arrayOne[i] < 0 && arrayTwo[j] < 0
-			)	{
-				diff = Math.abs(arrayOne[i] - arrayTwo[j])
-			}	else {
-				diff = Math.abs(arrayOne[i]) + Math.abs(arrayTwo[i]);
-			}
-			if (diff < min) {
-				min = diff;
-				result = [arrayOne[i], arrayTwo[j]];
-			}
-		}
-	}	
-	return result;
+    let min = Infinity;
+    let result = [];
+
+    for (let i = 0; i < arrayOne.length; i++) {
+        let diff;
+        for (let j = 0; j < arrayTwo.length; j++) {
+            if (
+                (arrayOne[i] >= 0 && arrayTwo[j] >= 0) ||
+                (arrayOne[i] < 0 && arrayTwo[j] < 0)
+            ) {
+                diff = Math.abs(arrayOne[i] - arrayTwo[j]);
+            } else {
+                diff = Math.abs(arrayOne[i]) + Math.abs(arrayTwo[i]);
+            }
+            if (diff < min) {
+                min = diff;
+                result = [arrayOne[i], arrayTwo[j]];
+            }
+        }
+    }
+    return result;
 }
