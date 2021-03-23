e1fa5dd0d1be7491aad98f92ba07c03343fbfd8f
pmin825 <pmin.0825@gmail.com>
2021-03-23 16:56:08 -0700

    first duplicate value 0n time 01 space

--- first_duplicate_value.js
+++ first_duplicate_value.js
@@ -1,22 +1,7 @@
-// O(n) time O(n) space
-
 function firstDuplicateValue(array) {
-  let hash = {};
-	
-	for (const num of array) {
-		if (hash[num]) return num;
-		hash[num] = true;
-	}
-	return -1
-}
-
-
-// O(n) time O(1) space
-
-function firstDuplicateValue(array) {
-	for (const num of array) {
-		if (array[Math.abs(num) - 1] < 0) return Math.abs(num);
-			array[Math.abs(num) - 1] *= -1;
-	}
-	return -1
+    for (const num of array) {
+        if (array[Math.abs(num) - 1] < 0) return Math.abs(num);
+        array[Math.abs(num) - 1] *= -1;
+    }
+    return -1;
 }
