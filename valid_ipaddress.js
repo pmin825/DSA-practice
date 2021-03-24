7ef7cc50cee2c36b4cf39dba72098ed34c8608e4
pmin825 <pmin.0825@gmail.com>
2021-03-23 20:05:09 -0700

    valid ip address

--- valid_ipaddress.js
+++ valid_ipaddress.js
@@ -1,34 +1,26 @@
 function validIPAddresses(string) {
-  if (string.length < 4) return [];
-  let result = [];
+    const results = [];
 
-  for (let i = 0; i < 4; i++) {
-    let iNum = parseInt(string.slice(0, i + 1));
-    let iString = string.slice(0, i + 1);
-    if (iNum >= 0 && iNum <= 255 && firstZero(iString)) {
-      for (let j = i + 1; j < i + 4; j++) {
-        let jNum = parseInt(string.slice(i + 1, j + 1));
-        let jString = string.slice(i + 1, j + 1);
-        if (jNum >= 0 && jNum <= 255 && firstZero(jString)) {
-          for (let k = j + 1; k < j + 4; k++) {
-            let kNum = parseInt(string.slice(j + 1, k + 1));
-            let kString = string.slice(j + 1, k + 1);
-            if (kNum >= 0 && kNum <= 255 && firstZero(kString)) {
-              let lNum = parseInt(string.slice(k + 1));
-              let lString = string.slice(k + 1);
-              if (lNum >= 0 && lNum <= 255 && firstZero(lString)) {
-                let address = iNum + "." + jNum + "." + kNum + "." + lNum;
-                result.push(address);
-              }
+    for (let i = 0; i < 3; i++) {
+        let num1 = string.slice(0, i + 1);
+        if (parseInt(num1) > 255 || !numChecker(num1)) continue;
+        for (let j = i + 1; j < i + 4; j++) {
+            let num2 = string.slice(i + 1, j + 1);
+            console.log(num2);
+            if (parseInt(num2) > 255 || !numChecker(num2)) continue;
+            for (let k = j + 1; k < j + 4; k++) {
+                let num3 = string.slice(j + 1, k + 1);
+                if (parseInt(num3) > 255 || !numChecker(num3)) continue;
+                let num4 = string.slice(k + 1);
+                if (parseInt(num4) > 255 || !numChecker(num4)) continue;
+                results.push(num1 + "." + num2 + "." + num3 + "." + num4);
             }
-          }
         }
-      }
     }
-  }
-  return result;
+    return results;
 }
 
-function firstZero(string) {
-  return parseInt(string).toString().length === string.length;
+function numChecker(string) {
+    if (parseInt(string).toString().length === string.length) return true;
+    return false;
 }
