a8ee828064bf957d9eba6c8f2ffeafe7c3194c13
pmin825 <pmin.0825@gmail.com>
2021-03-25 16:44:04 -0700

    find successor

--- find_successor.js
+++ find_successor.js
@@ -1,46 +1,33 @@
-// This is an input class. Do not edit.
-// O(h) time | O(1) space - where h is the height of the tree
 class BinaryTree {
     constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-      this.parent = null;
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
     }
-  }
-  
-  function findSuccessor(tree, node) {
-    if (node.right !== null) return getLeftMostChild(node.right);
-      return getRightmostParent(node);
-  }
-  
-  function getLeftMostChild(node) {
-      let currentNode = node;
-      while (currentNode.left !== null) {
-          currentNode = currentNode.left;
-      }
-      
-      return currentNode;
-  }
-  
-  function getRightmostParent(node) {
-      let currentNode = node;
-      while (currentNode.parent !== null && currentNode.parent.right === currentNode) {
-          currentNode = currentNode.parent;
-      }
-      
-      return currentNode.parent;
-  }
-  
-  // Do not edit the lines below.
-  exports.BinaryTree = BinaryTree;
-  exports.findSuccessor = findSuccessor;
-  
-for (const ele of array)
+}
 
-if (ele in hash)
+function findSuccessor(tree, node) {
+    if (node.right !== null) return leftMostChild(node.right);
+    return rightMostChild(node);
+}
 
+function leftMostChild(node) {
+    let currentNode = node;
+    while (currentNode.left != null) {
+        currentNode = currentNode.left;
+    }
+    return currentNode;
+}
 
-for (let i = [key, val]; i < Object.entries(hash); i++) {
-  
+function rightMostChild(node) {
+    let currentNode = node;
+    while (
+        currentNode.parent !== null &&
+        currentNode.parent.right === currentNode
+    ) {
+        currentNode = currentNode.parent;
+    }
+
+    return currentNode.parent;
 }
