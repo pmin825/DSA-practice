62d09a08b188780a05960c81a76e3e01ccd53d9b
pmin825 <pmin.0825@gmail.com>
2021-03-26 16:19:38 -0700

    bulb switcher 3 alternate solution

--- bulb_switcher_3.js
+++ bulb_switcher_3.js
@@ -17,3 +17,19 @@ var numTimesAllBlue = function (light) {
     }
     return momentsTurnedOn;
 };
+
+
+//alternate solution
+//O(n) time O(1) space
+var numTimesAllBlue = function(light) {
+    let moments = 0;
+    let total = 0;
+    let target = 0;
+    for (let i = 0; i < light.length; i++) {
+        total += light[i]
+        target += i + 1
+        
+        if (total === target) moments++
+    }
+    return moments;
+};
