c18f6b2a7e3aa0f71a587e917521de3806abea76
pmin825 <pmin.0825@gmail.com>
2021-03-27 14:43:18 -0700

    min num of coins for change

--- min_number_of_coins_for_change.js
+++ min_number_of_coins_for_change.js
@@ -1,15 +1,18 @@
 // O(nd) time | O(n) space
 
 function minNumberOfCoinsForChange(n, denoms) {
-    let coins = new Array(n + 1).fill(Infinity);
-    coins[0] = 0;
+    const numCoins = new Array(n + 1).fill(Infinity);
+    numCoins[0] = 0;
 
     for (const denom of denoms) {
-        for (let i = 0; i < coins.length; i++) {
-            if (denom <= i) {
-                coins[i] = Math.min(coins[i], coins[i - denom] + 1);
+        for (let amount = 1; amount <= n; amount++) {
+            if (denom <= amount) {
+                numCoins[amount] = Math.min(
+                    numCoins[amount],
+                    numCoins[amount - denom] + 1
+                );
             }
         }
     }
-    return coins[n] !== Infinity ? coins[n] : -1;
+    return numCoins[n] === Infinity ? -1 : numCoins[n];
 }
