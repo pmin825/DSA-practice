678bd206871197c5ef4c53c5c629996a60d90961
pmin825 <pmin.0825@gmail.com>
2021-03-27 17:43:21 -0700

    bfs

--- breadth_first_search.js
+++ breadth_first_search.js
@@ -1,26 +1,24 @@
-// Do not edit the class below except
-// for the breadthFirstSearch method.
-// Feel free to add new properties
-// and methods to the class.
 class Node {
-  constructor(name) {
-    this.name = name;
-    this.children = [];
-  }
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
 
-  addChild(name) {
-    this.children.push(new Node(name));
-    return this;
-  }
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
 
-  breadthFirstSearch(array) {
-		let q = [this];
-		while (q.length) {
-			let node = q.shift();
-			array.push(node.name);
-			for (const child of node.children) {
-				q.push(child);
-			}
-		}
-		return array;
-  }
+    breadthFirstSearch(array) {
+        array.push(this.name);
+        const queue = [this];
+        while (queue.length) {
+            const current = queue.shift();
+            for (const child of current.children) {
+                array.push(child.name);
+                queue.push(child);
+            }
+        }
+        return array;
+    }
+}
