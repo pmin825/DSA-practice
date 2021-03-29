2efdaca9da1f3c6ac63cc33acf93a8c6170297e5
pmin825 <pmin.0825@gmail.com>
2021-03-29 15:17:50 -0700

    ones and zeroes

---
+++ ones_and_zeroes.js
@@ -0,0 +1,16 @@
+const findMaxForm = (strs, m, n) => {
+    let dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+    for (const s of strs) {
+        let zero = 0,
+            one = 0;
+        for (const c of s) {
+            c === "0" ? zero++ : one++;
+        }
+        for (let i = m; i >= zero; i--) {
+            for (let j = n; j >= one; j--) {
+                dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1);
+            }
+        }
+    }
+    return dp[m][n];
+};
