d674df6133e612fdeb20f6af17c6fb3013a63807
pmin825 <pmin.0825@gmail.com>
2021-03-29 20:15:55 -0700

    number of ways to make change

--- number_of_ways_to_make_change.js
+++ number_of_ways_to_make_change.js
@@ -1,20 +1,15 @@
 // O(nd) time | O(n) space
 
 function numberOfWaysToMakeChange(n, denoms) {
-  //7 [2,3,4,7]
-  // Write your code here.
-  let ways = new Array(n + 1).fill(0); //[1,0,1,1,2,2,3,3]
-  ways[0] = 1;
-  for (let i = 0; i < denoms.length; i++) {
-    //3
-    for (let amount = 1; amount < n + 1; amount++) {
-      //7
-      if (denoms[i] <= amount) {
-        ways[amount] += ways[amount - denoms[i]]; //ways[7] += ways[0] = 2
-      }
+    const ways = new Array(n + 1).fill(0);
+    ways[0] = 1;
+
+    for (const denom of denoms) {
+        for (let i = 1; i <= n; i++) {
+            if (denom <= i) {
+                ways[i] += ways[i - denom];
+            }
+        }
     }
-  }
-  return ways[n];
+    return ways[n];
 }
-// Do not edit the line below.
-exports.numberOfWaysToMakeChange = numberOfWaysToMakeChange;
--- ones_and_zeroes.js
+++ ones_and_zeroes.js
@@ -1,3 +1,5 @@
+strs = ["10","0001","111001","1","0"], m = 5, n = 3
+
 const findMaxForm = (strs, m, n) => {
     let dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
     for (const s of strs) {
@@ -6,6 +8,7 @@ const findMaxForm = (strs, m, n) => {
         for (const c of s) {
             c === "0" ? zero++ : one++;
         }
+        
         for (let i = m; i >= zero; i--) {
             for (let j = n; j >= one; j--) {
                 dp[i][j] = Math.max(dp[i][j], dp[i - zero][j - one] + 1);
@@ -14,3 +17,5 @@ const findMaxForm = (strs, m, n) => {
     }
     return dp[m][n];
 };
+
+findMaxForm(strs, m, n)
