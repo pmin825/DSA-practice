f4b93a18b1b52ee37dfb2ed5f27b51af515de667
pmin825 <pmin.0825@gmail.com>
2021-04-06 16:38:53 -0700

    merge overlapping intervals

---
+++ merge_overlapping_intervals.js
@@ -0,0 +1,28 @@
+function mergeOverlappingIntervals(array) {
+	array.sort((a, b) => a[0] - b[0]);
+	const results = [];
+	let start = array[0][0];
+	let end = array[0][1];
+	
+	for (let i = 0; i < array.length; i++) {
+		let checkStart = array[i][0];
+		let checkEnd = array[i][1];
+		
+		if (checkStart <= end) {
+			if (checkEnd > end) {
+				end = checkEnd;
+			}
+		}
+		
+		if (checkStart > end) {
+			results.push([start, end]);
+			start = checkStart;
+			end = checkEnd;
+		}
+		
+		if (i === array.length - 1) {
+			results.push([start, end])
+		}
+	}
+	return results;
+}
