d7c3f57baf511569866563c3c3f6416e6f853146
pmin825 <pmin.0825@gmail.com>
2021-04-06 17:27:03 -0700

    levenshtein distance

--- levenshtein_distance.js
+++ levenshtein_distance.js
@@ -1,28 +1,25 @@
 // O(nm) time | O(nm) space
 
-function levenshteinDistance(str1, str2) {	
-	let edits = [];
-	for (let i = 0 ; i < str2.length + 1; i++) {
-		const row = [];
-		for (let j = 0; j < str1.length + 1; j++) {
-			row.push(j);
-		}
-		row[0] = i;
-		edits.push(row);
-	}
-	console.log(edits);
-	
-	for (let i = 1; i < str2.length + 1; i++) {
-		for (let j = 1; j < str1.length + 1; j++) {
-			if (str2[i - 1] === str1[j - 1]) {
-				edits[i][j] = edits[i - 1][j - 1];
-			}	else {
-				edits[i][j] = 1 + Math.min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j-1]);
-			}
-		}
-	}
-	console.log(edits);
-	return edits[str2.length][str1.length]
-}
+function levenshteinDistance(str1, str2) {
+    const dp = [];
+    for (let i = 0; i < str2.length + 1; i++) {
+        let row = [];
+        for (let j = 0; j < str1.length + 1; j++) {
+            row.push(j);
+        }
+        row[0] = i;
+        dp.push(row);
+    }
 
-levenshteinDistance('abc', 'yabd')
+    for (let i = 1; i < str2.length + 1; i++) {
+        for (let j = 1; j < str1.length + 1; j++) {
+            if (str2[i - 1] === str1[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] =
+                    1 + Math.min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return dp[str2.length][str1.length];
+}
