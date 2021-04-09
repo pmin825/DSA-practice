b0cf4fd0f28e87d9db45f885eb7a7eb31bc8e3bb
pmin825 <pmin.0825@gmail.com>
2021-04-09 12:57:11 -0700

    validate three nodes

--- same_bsts.js
+++ same_bsts.js
@@ -1,27 +1,27 @@
 function sameBsts(arrayOne, arrayTwo) {
-    if (arrayOne.length !== arrayTwo.length) return false;
-    if (arrayOne[0] !== arrayTwo[0]) return false;
-    if (!arrayOne.length & !arrayTwo.length) return true;
-    const leftOne = getSmaller(arrayOne);
-    const leftTwo = getSmaller(arrayTwo);
-    const rightOne = getBiggerOrEqual(arrayOne);
-    const rightTwo = getBiggerOrEqual(arrayTwo);
+  if (arrayOne.length !== arrayTwo.length) return false;
+  if (arrayOne[0] !== arrayTwo[0]) return false;
+  if (!arrayOne.length & !arrayTwo.length) return true;
+  const leftOne = getSmaller(arrayOne);
+  const leftTwo = getSmaller(arrayTwo);
+  const rightOne = getBiggerOrEqual(arrayOne);
+  const rightTwo = getBiggerOrEqual(arrayTwo);
 
-    return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo);
+  return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo);
 }
 
 function getSmaller(array) {
-    const smaller = [];
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] < array[0]) smaller.push(array[i]);
-    }
-    return smaller;
+  const smaller = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[0]) smaller.push(array[i]);
+  }
+  return smaller;
 }
 
 function getBiggerOrEqual(array) {
-    const biggerOrEqual = [];
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] >= array[0]) biggerOrEqual.push(array[i]);
-    }
-    return biggerOrEqual;
+  const biggerOrEqual = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] >= array[0]) biggerOrEqual.push(array[i]);
+  }
+  return biggerOrEqual;
 }
