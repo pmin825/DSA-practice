b0e4c879b23a0a8981aef482bc9db94d88e7b9f5
pmin825 <pmin.0825@gmail.com>
2021-04-09 08:00:30 -0700

    zig zag traverse

---
+++ zigzag_traverse.js
@@ -0,0 +1,43 @@
+function zigzagTraverse(array) {
+    const result = [];
+    const height = array.length - 1;
+    const width = array[0].length - 1;
+    let row = 0;
+    let col = 0;
+    let goingDown = true;
+
+    while (!isOutOfBounds(row, col, height, width)) {
+        result.push(array[row][col]);
+        if (goingDown) {
+            if (col === 0 || row === height) {
+                goingDown = false;
+                if (row === height) {
+                    col++;
+                } else {
+                    row++;
+                }
+            } else {
+                row++;
+                col--;
+            }
+        } else {
+            if (row === 0 || col === width) {
+                goingDown = true;
+                if (col === width) {
+                    row++;
+                } else {
+                    col++;
+                }
+            } else {
+                row--;
+                col++;
+            }
+        }
+    }
+
+    return result;
+}
+
+function isOutOfBounds(row, col, height, width) {
+    return row > height || row < 0 || col > width || col < 0;
+}
