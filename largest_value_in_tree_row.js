93db33ae89af7154cdc7a17b69fb51ea4a26caa9
pmin825 <pmin.0825@gmail.com>
2021-04-09 12:07:18 -0700

    largest value in tree row

---
+++ largest_value_in_tree_row.js
@@ -0,0 +1,18 @@
+const largestValues = (root) => {
+    if (!root) return [];
+    const largest = [];
+    const queue = [root];
+    while (queue.length) {
+        let max = -Infinity;
+        const length = queue.length;
+        for (let i = 0; i < length; i++) {
+            let node = queue.shift();
+            if (node.val > max) max = node.val;
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        largest.push(max);
+    }
+
+    return largest;
+};
