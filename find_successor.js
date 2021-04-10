9b6af819fdc0ed35f11495ea011d1f375b5f68cd
pmin825 <pmin.0825@gmail.com>
2021-04-10 12:08:28 -0700

    find successor

--- find_successor.js
+++ find_successor.js
@@ -1,30 +1,29 @@
-// This is an input class. Do not edit.
 class BinaryTree {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
 }
 
 function findSuccessor(tree, node) {
-    if (node.right !== null) return leftMostChild(node.right);
-    return rightMostParent(node);
-}
-
-function leftMostChild(node) {
-    let current = node;
-    while (current.left !== null) {
-        current = current.left;
-    }
-    return current;
+  if (node.right !== null) return leftMostChild(node.right);
+  return rightMostParent(node);
 }
 
 function rightMostParent(node) {
-    let current = node;
-    while (current.parent !== null && current.parent.right === node) {
-        current = current.parent;
-    }
-    return current.parent;
+  let current = node;
+  if (current.parent !== null && current.parent.right === current) {
+    current = current.parent;
+  }
+  return current.parent;
+}
+
+function leftMostChild(node) {
+  let current = node;
+  while (current.left !== null) {
+    current = current.left;
+  }
+  return current;
 }
