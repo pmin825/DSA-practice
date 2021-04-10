8c75dbd23b20dd7848b4aa4652a87ae8634da941
pmin825 <pmin.0825@gmail.com>
2021-04-10 11:43:58 -0700

    binary tree diameter

--- binary_tree_diameter.js
+++ binary_tree_diameter.js
@@ -3,36 +3,35 @@
 // where n is the number of nodes, h is the height of the binary tree.
 
 class BinaryTree {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
 class TreeInfo {
-    constructor(diameter, height) {
-        this.diameter = diameter;
-        this.height = height;
-    }
+  constructor(diameter, height) {
+    this.diameter = diameter;
+    this.height = height;
+  }
 }
 
 function binaryTreeDiameter(tree) {
-    return getTreeInfo(tree).diameter;
+  return getTreeInfo(tree).diameter;
 }
 
 function getTreeInfo(tree) {
-    if (tree === null) return new TreeInfo(0, 0);
+  if (tree === null) return new TreeInfo(0, 0);
+  const leftTreeInfo = getTreeInfo(tree.left);
+  const rightTreeInfo = getTreeInfo(tree.right);
+  const longestPathThruRoot = leftTreeInfo.height + rightTreeInfo.height;
+  const currentMaxDiameter = Math.max(
+    longestPathThruRoot,
+    leftTreeInfo.diameter,
+    rightTreeInfo.diameter
+  );
+  const currentHeight = Math.max(leftTreeInfo.height, rightTreeInfo.height);
 
-    const leftSubtree = getTreeInfo(tree.left);
-    const rightSubtree = getTreeInfo(tree.right);
-    const diameterThruRoot = leftSubtree.height + rightSubtree.height;
-    const maxDiameter = Math.max(
-        diameterThruRoot,
-        leftSubtree.diameter,
-        rightSubtree.diameter
-    );
-    const currentHeight = Math.max(leftSubtree.height, rightSubtree.height);
-
-    return new TreeInfo(maxDiameter, currentHeight + 1);
+  return new TreeInfo(currentMaxDiameter, currentHeight + 1);
 }
--- reverse_linked_list.js
+++ reverse_linked_list.js
@@ -1,12 +1,28 @@
-const reverseList = (head) => {
-    let prev = null;
-    while (head) {
-        const next = head.next;
-        const curr = head;
-        curr.next = prev;
-        head = next;
-        prev = curr;
-    }
+// const reverseList = (head) => {
+//     let prev = null;
+//     while (head) {
+//         const next = head.next;
+//         const curr = head;
+//         curr.next = prev;
+//         head = next;
+//         prev = curr;
+//     }
 
-    return prev;
-};
+//     return prev;
+// };
+
+function reverseLinkedList(head) {
+	let node = head;
+	const values = [];
+	while (node.next !== null) {
+		values.push(node);
+		node = node.next;
+	}
+	head = node;
+	while (values.length) {
+		node.next = values.pop();
+		node = node.next;
+	}
+	node.next = null;
+	return head;
+}
