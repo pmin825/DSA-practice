75810a5a17cddb6f87cbec9562de8c417e4dbc05
pmin825 <pmin.0825@gmail.com>
2021-04-20 11:45:56 -0700

    index equals value alternate solution

--- index_equals_value.js
+++ index_equals_value.js
@@ -1,7 +1,21 @@
+// O(log(n)) time | O(1) space
+
 function indexEqualsValue(array) {
-  for (let i = 0; i < array.length; i++) {
-    let num = array[i];
-    if (num === i) return num;
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+
+    if (array[mid] < mid) {
+      left = mid + 1;
+    } else if (array[mid] === mid && mid === 0) {
+      return mid;
+    } else if (array[mid] === mid && array[mid - 1] < mid - 1) {
+      return mid;
+    } else {
+      right = mid - 1;
+    }
   }
   return -1;
 }
