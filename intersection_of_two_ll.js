11585f0ce86c87218dbe12fe4ede935f2a791598
pmin825 <pmin.0825@gmail.com>
2021-05-07 14:05:16 -0700

    intersection of two ll

---
+++ intersection_of_two_ll.js
@@ -0,0 +1,12 @@
+const getIntersectionNode = (headA, headB) => {
+    if (!headA || !headB) return null;
+    let p1 = headA;
+    let p2 = headB;
+
+    while (p1 !== p2) {
+        p1 = p1 === null ? headB : p1.next;
+        p2 = p2 === null ? headA : p2.next;
+    }
+
+    return p1;
+};
