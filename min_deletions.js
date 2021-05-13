cc2eca8093ae038e8d7983fcedff2604cd01015e
pmin825 <pmin.0825@gmail.com>
2021-05-13 15:48:17 -0700

    min deletions

---
+++ min_deletions.js
@@ -0,0 +1,23 @@
+const minDeletions = (s) => {
+  const count = {};
+  let deletions = 0;
+
+  for (const char of s) {
+    if (!count[char]) {
+      count[char] = 1;
+    } else {
+      count[char] += 1;
+    }
+  }
+
+  const unique = new Set();
+  for (const key in count) {
+    while (unique.has(count[key])) {
+      console.log(unique, count[key]);
+      deletions++;
+      count[key] -= 1;
+    }
+    if (count[key] > 0) unique.add(count[key]);
+  }
+  return deletions;
+};
