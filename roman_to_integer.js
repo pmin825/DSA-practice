23b09e3561f6e30192d2e7b75ec5c171d825e78c
pmin825 <pmin.0825@gmail.com>
2021-05-18 12:02:06 -0700

    roman to integer

---
+++ roman_to_integer.js
@@ -0,0 +1,24 @@
+const romanToInt = (s) => {
+  const values = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    const current = s[i];
+    const next = s[i + 1];
+
+    if (values[current] < values[next]) {
+      total -= values[current];
+    } else {
+      total += values[current];
+    }
+  }
+  return total;
+};
