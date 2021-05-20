87ab78dab019be48ee06c390595e4fa93ecbc945
pmin825 <pmin.0825@gmail.com>
2021-05-20 14:13:27 -0700

    top k frequent elements alternate solution

--- top_k_frequent_elements.js
+++ top_k_frequent_elements.js
@@ -1,3 +1,25 @@
+// O(n)
+const topKFrequent = (nums, k) => {
+    const count = {};
+    const bucket = [];
+    const result = [];
+
+    for (const num of nums) {
+        count[num] = count[num] + 1 || 1;
+    }
+
+    for (const [val, freq] of Object.entries(count)) {
+        bucket[freq] = (bucket[freq] || new Set()).add(val);
+    }
+
+    for (let i = bucket.length - 1; i >= 0; i--) {
+        if (bucket[i]) result.push(...bucket[i]);
+        if (result.length === k) break;
+    }
+    return result;
+};
+
+// O(n log n)
 const topKFrequent = (nums, k) => {
     const count = {};
     for (const num of nums) {
