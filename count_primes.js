98cee2d79fb4e39987d90c83e942e0675d8f5cab
pmin825 <pmin.0825@gmail.com>
2021-05-24 08:57:32 -0700

    count primes

---
+++ count_primes.js
@@ -0,0 +1,13 @@
+const countPrimes = (n) => {
+    const dp = new Array(n).fill(true);
+    dp[0] = false;
+    dp[1] = false;
+    for (let i = 2; i * i < n; i++) {
+        if (dp[i]) {
+            for (let j = i * i; j < n; j += i) {
+                dp[j] = false;
+            }
+        }
+    }
+    return dp.filter((val) => val).length;
+};
