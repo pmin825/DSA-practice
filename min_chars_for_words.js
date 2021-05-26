bcabd3a1ed0050fea4e4ada85dda58b5efb06ad1
pmin825 <pmin.0825@gmail.com>
2021-05-25 21:01:48 -0700

    min chars for words

---
+++ min_chars_for_words.js
@@ -0,0 +1,33 @@
+function minimumCharactersForWords(words) {
+  const count = {};
+  const result = [];
+
+  for (const word of words) {
+    const temp = {};
+    for (let i = 0; i < word.length; i++) {
+      if (!temp[word[i]]) {
+        temp[word[i]] = 1;
+      } else {
+        temp[word[i]] += 1;
+      }
+    }
+
+    for (const key in temp) {
+      if (!count[key]) {
+        count[key] = temp[key];
+      } else {
+        count[key] = Math.max(count[key], temp[key]);
+      }
+    }
+  }
+
+  for (const [key, value] of Object.entries(count)) {
+    let counter = value;
+    while (counter > 0) {
+      result.push(key);
+      counter--;
+    }
+  }
+
+  return result;
+}
