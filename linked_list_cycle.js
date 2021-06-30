f78930a2a36299b41be9458401547b7efe3e72c4
pmin825 <pmin.0825@gmail.com>
2021-06-29 21:04:51 -0700

    linked list cycle

--- linked_list_cycle.js
+++ linked_list_cycle.js
@@ -1,13 +1,13 @@
-const hasCycle = (head) => {
-    let slow = head;
-    let fast = head;
+var hasCycle = function (head) {
+  let slow = head,
+    fast = head;
 
-    while (fast && fast.next && fast.next.next) {
-        slow = slow.next;
-        fast = fast.next.next;
+  while (fast && fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
 
-        if (slow === fast) return true;
-    }
+    if (slow === fast) return true;
+  }
 
-    return false;
+  return false;
 };
